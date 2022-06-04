d910a7bb70b4feeba8f1081ba4f597cd05ef9fb6
Jakub Musialowski <mds@mds.biz.pl>
2022-06-04 22:33:46 +0200

    Improved style changing, light/black layout.

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -7,20 +7,34 @@ let web = document.querySelector(".text1");
 let code = document.querySelector(".text2");
 let footer = document.querySelector(".footer");
 let change = document.querySelector(".button");
+let menu = document.querySelector(".menu");
+let menuLink = document.querySelectorAll(".menu__link");
+let rowHead = document.querySelectorAll(".onlineBuyer__rowHead");
+let colHead = document.querySelectorAll(".onlineBuyer__colHead");
+let colSummary = document.querySelector(".onlineBuyer__colSummary");
 
-change.addEventListener("click", () =>{
+change.addEventListener("click", () => {
     page.classList.toggle("blackStyle");
     header.classList.toggle("blackStyle");
     text.classList.toggle("blackStyle");
     footer.classList.toggle("blackStyle");
     web.classList.toggle("blackStyle");
     code.classList.toggle("blackStyle");
-// if (page.classList.contains("blackStyle")){
-//     change.innerText = "Jasny";
-//         } else {
-//         change.innerText = "Ciemny";
-//     }
+    menu.classList.toggle("blackStyle");
+    menuLink.forEach((l) => {
+        l.classList.toggle("blackStyle");
+    })
+    rowHead.forEach((r) => {
+        r.classList.toggle("onlineBuyer__rowHead--black");
+    })
+    colHead.forEach((c) => {
+        c.classList.toggle("onlineBuyer__colHead--black");
+    })
+    colSummary.classList.toggle("onlineBuyer__colSummary--black");
+    // if (page.classList.contains("blackStyle")){
+    //     change.innerText = "Jasny";
+    //         } else {
+    //         change.innerText = "Ciemny";
+    //     }
     change.innerText = page.classList.contains("blackStyle") ? "Jasny" : "Ciemny";
 })
-
-typeof text
